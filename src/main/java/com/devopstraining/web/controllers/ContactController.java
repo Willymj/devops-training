1af96d37cfc4f2a72ba5fdda19a445ac77aa9d7d
William <w.mouangue@gmail.com>
2018-04-05 22:48:31 +0200

    contact us page part 1

---
+++ src/main/java/com/devopstraining/web/controllers/ContactController.java
@@ -0,0 +1,36 @@
+package com.devopstraining.web.controllers;
+
+import com.devopstraining.web.domain.frontend.FeedbackPojo;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class ContactController {
+
+    /** The application logger **/
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(ContactController.class);
+
+    /** The key which identifies the feedback payload in the Model **/
+    public static final String FEEDBACK_MODEL_KEY = "feedback";
+
+    /** The contact us view name **/
+    public static final String CONTACT_US_VIEW_NAME = "contact/contact";
+
+    @RequestMapping(value = "/contact", method = RequestMethod.GET)
+    public String contactGet(ModelMap model){
+        FeedbackPojo feedbackPojo = new FeedbackPojo();
+        model.addAttribute(ContactController.FEEDBACK_MODEL_KEY, feedbackPojo);
+        return ContactController.CONTACT_US_VIEW_NAME;
+    }
+
+    @RequestMapping(value = "/contact", method = RequestMethod.POST)
+    public String contactPost(@ModelAttribute(FEEDBACK_MODEL_KEY) FeedbackPojo feedback){
+        LOG.debug("Feedback pojo content: {}", feedback);
+        return ContactController.CONTACT_US_VIEW_NAME;
+    }
+}
