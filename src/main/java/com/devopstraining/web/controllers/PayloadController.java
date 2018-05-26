1e6054e9d2098671609178c1f83363c732c9c382
William <w.mouangue@gmail.com>
2018-05-26 21:01:54 +0200

    created a payload page

---
+++ src/main/java/com/devopstraining/web/controllers/PayloadController.java
@@ -0,0 +1,14 @@
+package com.devopstraining.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PayloadController {
+    /** The payload view name **/
+    public static final String PAYLOAD_VIEW_NAME = "payload/payload";
+
+    @RequestMapping("/payload")
+    public String payload(){ return PAYLOAD_VIEW_NAME; }
+
+}
